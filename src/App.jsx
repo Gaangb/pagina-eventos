4d643a19b0ca228641a5cd3dd2d8c9ff87f6c401
Gaangb <angelob4stos@gmail.com>
2024-02-07 16:25:06 -0300

    feat: adicionando provider e rotas de registro e reset ao app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
 import './App.css'
+import { ResetPage } from './pages/ResetPage'
+import { EventProvider } from './hooks/useEventsBuilder'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +12,13 @@ function App() {
   return (
     <Router>
       <div className="container_geral">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-        </Routes>
+        <EventProvider>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/reset" element={<ResetPage />} />
+          </Routes>
+        </EventProvider>
       </div>
     </Router>
   )
