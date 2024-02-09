ef08a9a4769297354b0e5e9b75262b081a7e2017
Gaangb <angelob4stos@gmail.com>
2024-02-09 11:19:25 -0300

    feat: iniiciando criacao do componente card

---
+++ src/components/CardEvent/index.jsx
@@ -0,0 +1,8 @@
+export default function CardEvent() {
+
+    return (
+        <div className="cardEvent">
+            <img src="https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="" />
+        </div>
+    )
+}
