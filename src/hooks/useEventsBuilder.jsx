40a879fc995a85410351c2e561ac4485ef7a083b
Gaangb <angelob4stos@gmail.com>
2024-02-09 16:01:33 -0300

    feat: criando usestate para eventos

--- src/hooks/useEventsBuilder.jsx
+++ src/hooks/useEventsBuilder.jsx
@@ -1,27 +1,42 @@
-import {
-    createContext,
-    useContext,
-    useState,
-    useEffect,
-    useMemo,
-} from "react";
+import { createContext, useContext, useState } from "react";
 
 const EventContext = createContext();
 
 export function EventProvider({ children }) {
+  const [usuarios, setUsuarios] = useState([
+    {
+      id: 1,
+      email: "administrador@gmail.com",
+      senha: "senha123",
+      cpf: "12345678900",
+      nome: "admin",
+    },
+  ]);
 
-    const [usuarios, setUsuarios] = useState([{ id: 1, email: 'administrador@gmail.com', senha: 'senha123', cpf: '12345678900', nome: 'admin' }]);
+  const [eventos, setEventos] = useState([
+    {
+      id: 1,
+      nome: "Rock Rio",
+      local: "Rio de Janeiro",
+      data: "10/10/2021",
+      horario: "20:00",
+    },
+    {
+        id: 2,
+        nome: "Rock  in Rio",
+        local: "Rio de Janeiro",
+        data: "10/10/2021",
+        horario: "20:00",
+      }
 
-    return (
-        <EventContext.Provider value={{usuarios, setUsuarios}}>
-            {children}
-        </EventContext.Provider>
-    );
+  ])
+  return (
+    <EventContext.Provider value={{ usuarios, setUsuarios, eventos, setEventos }}>
+      {children}
+    </EventContext.Provider>
+  );
 }
 
 export function useEventsBuilder() {
-
-    return useContext(EventContext);
+  return useContext(EventContext);
 }
-
-
