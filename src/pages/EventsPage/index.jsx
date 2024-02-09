932941fdf25e9b8827f5e844a1a6f1c1f724f6bd
Gaangb <angelob4stos@gmail.com>
2024-02-09 16:02:31 -0300

    feat: renderizando cardevents com map

--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -1,17 +1,30 @@
 import CardEvent from "../../components/CardEvent";
 import styles from "./styles.module.css";
+import { useEventsBuilder } from "../../hooks/useEventsBuilder";
 
 export function EventsPage() {
-    return (
-        <section className={styles.container_geral_events_page}>
-            <div className={styles.container_navbar_events_page}>
-                <input type="text" name="" id="" placeholder="Pesquise" />
-                <a href=""><p>minha conta</p></a>
-                <button>Sair</button>
-            </div>
-            <div>
-                <CardEvent />
-            </div>
-        </section>
-    );
+    const { eventos } = useEventsBuilder();
+
+
+  return (
+    <section className={styles.container_geral_events_page}>
+      <div className={styles.container_navbar_events_page}>
+        <input type="text" name="" id="" placeholder="Pesquise" />
+        <a href="">
+          <p>criar evento</p>
+        </a>
+        <a href="">
+          <p>minha conta</p>
+        </a>
+        <button>Sair</button>
+      </div>
+      <div className={styles.container_events_page}>
+        <div>
+        {eventos.map((event, index) => (
+            <CardEvent key={index} {...event} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
