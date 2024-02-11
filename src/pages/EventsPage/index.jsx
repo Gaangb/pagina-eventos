79f14f3ede20d1cc53a2dc9cdd500e6eddaac391
Gaangb <angelob4stos@gmail.com>
2024-02-10 22:35:31 -0300

    feat: adicionando bloqueio a tela de eventos quando form esta aberto

--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -1,30 +1,32 @@
 import CardEvent from "../../components/CardEvent";
 import styles from "./styles.module.css";
 import { useEventsBuilder } from "../../hooks/useEventsBuilder";
+import { useState } from "react";
+import { CreateEvent } from "../CreateEvent";
 
 export function EventsPage() {
-    const { eventos } = useEventsBuilder();
+  const { eventos, showForm, toggleForm } = useEventsBuilder();
 
 
   return (
     <section className={styles.container_geral_events_page}>
       <div className={styles.container_navbar_events_page}>
         <input type="text" name="" id="" placeholder="Pesquise" />
-        <a href="">
-          <p>criar evento</p>
-        </a>
-        <a href="">
-          <p>minha conta</p>
-        </a>
-        <button>Sair</button>
+        <button className={styles.button_navbar_events_page} onClick={toggleForm}>Criar evento</button>
+        <button className={styles.button_navbar_events_page}>Minha conta</button>
+        <button className={styles.button_sair_navbar_events_page}>Sair</button>
       </div>
-      <div className={styles.container_events_page}>
-        <div>
-        {eventos.map((event, index) => (
-            <CardEvent key={index} {...event} />
-          ))}
+      <div className={`${styles.container_events_page} `}>
+        <div className={`${styles.container_events_page} ${showForm ? styles.modal_open : ""}`}>
+
+      {showForm && <CreateEvent />}
         </div>
+      <div className={styles.container_cards_events_page}>
+        {eventos.map((event, index) => (
+          <CardEvent key={index} {...event} />
+        ))}
       </div>
-    </section>
+    </div>
+    </section >
   );
 }
