cf93132e2ef30da05d51a2561c5f4b106dae633b
Gaangb <angelob4stos@gmail.com>
2024-02-14 04:26:47 -0300

    fix: alterando ion icon

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -3,7 +3,9 @@ import styles from "./styles.module.css";
 export function UserPage() {
     return (
         <div className={styles.container_geral}>
-            <h1><ion-icon name="settings-outline"></ion-icon>Configurações do usuário</h1>
+            <div className={styles.container_titulo}>
+                <h1><ion-icon name="settings-sharp"></ion-icon>Configurações do usuário</h1>
+            </div>
         </div>
     );
 }
