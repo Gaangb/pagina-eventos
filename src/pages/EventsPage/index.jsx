67a57a9ff8db38c94ea4818aaaf1057254a3500f
Gaangb <angelob4stos@gmail.com>
2024-02-14 03:41:45 -0300

    ref: removendo navbar de pagina de eventos para tornar componente

--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -1,52 +1,26 @@
 import CardEvent from "../../components/CardEvent";
 import styles from "./styles.module.css";
 import { useEventsBuilder } from "../../hooks/useEventsBuilder";
-import { useState } from "react";
-import { CreateEvent } from "../CreateEvent";
-import logo from "../../assets/logo.png"
-
+import { CreateEvent } from "../../components/CreateEvent";
+import { NavBar } from "../../components/NavBar";
+import { useEffect } from "react";
 export function EventsPage() {
-  const { eventos, showForm, toggleForm, isLogged, setIsLogged } = useEventsBuilder();
+  const { eventos, showForm, setEventos} = useEventsBuilder();
 
-  let loggedInUser = null;
-
-  const loggedInUserJSON = localStorage.getItem("loggedInUser");
-
-  if (loggedInUserJSON) {
-    loggedInUser = JSON.parse(loggedInUserJSON);
-    setIsLogged(true)
-  }
+  useEffect(() => {
+    if (eventos.length) {
+      localStorage.setItem('eventos', JSON.stringify(eventos));
+    }
+  }, [eventos]);
   
-  const handleLogOut = () => {
-    setIsLogged(false)
-    localStorage.removeItem("loggedInUser");
-  }
-
-  const buttons = isLogged ? (
-    <>
-      <button className={styles.button_navbar_events_page}>Criar evento</button>
-      <button className={styles.button_navbar_events_page}>Minha conta</button>
-      <button className={styles.button_sair_navbar_events_page} onClick={() => {handleLogOut()}}>Sair</button>
-    </>
-  ) : (
-    <>
-      <button className={styles.button_login_navbar_events_page} onClick={() => location.href = '/register'}>Criar conta</button>
-      <button className={styles.button_login_navbar_events_page} onClick={() => location.href = '/login'}>Login</button>
-    </>
-  );
+  useEffect(() => {
+    const eventosLocalStorage = JSON.parse(localStorage.getItem('eventos'));
+    setEventos(eventosLocalStorage);
+  }, []);
 
   return (
     <section className={styles.container_geral_events_page}>
-      <div className={styles.container_navbar_events_page}>
-        <img src={logo} alt="Logo" />
-        <input type="text" name="" id="" placeholder="Pesquise" />
-        {buttons}
-      </div>
       <div className={`${styles.container_events_page} `}>
-        <div className={`${styles.container_events_page} ${showForm ? styles.modal_open : ""}`}>
-
-          {showForm && <CreateEvent />}
-        </div>
         <div className={styles.container_cards_events_page}>
           {eventos.map((event, index) => (
             <CardEvent key={index} {...event} />
