26594ddac9f235fa9f9a20a9d381898e0d69e04b
Gaangb <angelob4stos@gmail.com>
2024-02-14 03:38:32 -0300

    feat: criando pagina de usuario

---
+++ src/pages/UserPage/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export function UserPage() {
+    return (
+        <div className={styles.container_geral}>
+            <h1><ion-icon name="settings-outline"></ion-icon>Configurações do usuário</h1>
+        </div>
+    );
+}
