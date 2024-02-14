5939c2818e4333da0f05f3b20143812c9b0bafde
Gaangb <angelob4stos@gmail.com>
2024-02-14 03:44:48 -0300

    ref: adicionando customclass para navbar, corrigindo criação de eventos pre definidos, adicioando logica de mostrar formulario,

--- src/hooks/useEventsBuilder.jsx
+++ src/hooks/useEventsBuilder.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import logo from "../assets/logo_login.png"
 
 const EventContext = createContext();
@@ -6,6 +6,10 @@ const EventContext = createContext();
 export function EventProvider({ children }) {
 
   const [isLogged, setIsLogged] = useState(false);
+
+  const [showForm, setShowForm] = useState(false)
+
+  const [customClassNavBar, setCustomClassNavBar] = useState("container_navbar_events_page")
 
   const [usuarios, setUsuarios] = useState([
     {
@@ -15,63 +19,75 @@ export function EventProvider({ children }) {
       cpf: "12345678900",
       nome: "admin",
       nome_estabelecimento: "Feira Eventos",
-    },
+    },  
   ]);
 
-  const [eventos, setEventos] = useState([
+  const eventosPreDefinidos = [
     {
       id: 1,
       nome: "Rock Rio",
       local: "Rio de Janeiro",
       data: "10/10/2021",
       horario: "20:00",
-      preco: "R$ 20,00",
       imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
       descricao: "Rock in Rio",
     },
     {
-      id: 1,
+      id: 2,
       nome: "Rock Rio",
       local: "Rio de Janeiro",
       data: "10/10/2021",
       horario: "20:00",
-      preco: "R$ 20,00",
       imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
       descricao: "Rock in Rio",
+      ingressos_pista: 100,
+      ingressos_camarote: 100,
+      preco_pista: 100,
+      preco_camarote: 100,
     },
     {
-      id: 1,
+      id: 3,
       nome: "Rock Rio",
       local: "Rio de Janeiro",
       data: "10/10/2021",
       horario: "20:00",
-      preco: "R$ 20,00",
       imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
       descricao: "Rock in Rio",
-    },
-    {
-      id: 1,
-      nome: "Rock Rio",
-      local: "Rio de Janeiro",
-      data: "10/10/2021",
-      horario: "20:00",
-      preco: "R$ 20,00",
-      imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
-      descricao: "Rock in Rio",
-    },
-    {
-      id: 1,
-      nome: "Rock Rio",
-      local: "Rio de Janeiro",
-      data: "10/10/2021",
-      horario: "20:00",
-      preco: "R$ 20,00",
-      imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
-      descricao: "Rock in Rio",
-    },
-  ])
+      ingressos_pista: 100,
+      ingressos_camarote: 100,
+      preco_pista: 100,
+      preco_camarote: 100,
+    }
+  ];
 
-  const [showForm, setShowForm] = useState(false)
+  const [eventos, setEventos] = useState(() => {
+    const eventosLocalStorage = JSON.parse(localStorage.getItem('eventos'));
+    
+    if (eventosLocalStorage) {
+      const idsLocalStorage = new Set(eventosLocalStorage.map(evento => evento.id));
+      const eventosPreDefinidosNovos = eventosPreDefinidos.filter(evento => !idsLocalStorage.has(evento.id));
+      return [...eventosPreDefinidosNovos, ...eventosLocalStorage];
+    } else {
+      return eventosPreDefinidos;
+    }
+  });
+
+
+  useEffect(() => {
+    let loggedInUser = null;
+
+    const loggedInUserJSON = localStorage.getItem("loggedInUser");
+  
+    if (loggedInUserJSON) {
+        loggedInUser = JSON.parse(loggedInUserJSON);
+        setIsLogged(true)
+    }
+  }, [isLogged]);
+
+  const handleLogOut = () => {
+    setIsLogged(false)
+    localStorage.removeItem("loggedInUser");
+}
 
   const toggleForm = (e) => {
     e.preventDefault()
@@ -92,6 +108,9 @@ export function EventProvider({ children }) {
       isLogged,
       setIsLogged,
       showLogo,
+      handleLogOut,
+      customClassNavBar,
+      setCustomClassNavBar,
     }}>
       {children}
     </EventContext.Provider>
