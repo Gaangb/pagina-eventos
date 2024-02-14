9935d0100196e05584fabb1dbf1cff36c158c847
Gaangb <angelob4stos@gmail.com>
2024-02-14 03:35:36 -0300

    feat: adicionando rota de minha conta e adicionando a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
 import './App.css'
 import { ResetPage } from './pages/ResetPage'
 import { EventProvider } from './hooks/useEventsBuilder'
 import { EventsPage } from './pages/EventsPage'
-import { CreateEvent } from './pages/CreateEvent'
+import { NavBar } from './components/NavBar'
+import { UserPage } from './pages/UserPage'
 
 function App() {
 
@@ -14,12 +14,13 @@ function App() {
     <Router>
       <div className="container_geral">
         <EventProvider>
+        <NavBar />
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/reset" element={<ResetPage />} />
             <Route path="/" element={<EventsPage />} />
-            <Route path="/create-event" element={<CreateEvent />} />
+            <Route path="/minha-conta" element={<UserPage />} />
           </Routes>
         </EventProvider>
       </div>
