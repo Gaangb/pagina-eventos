7a4b72578676256c45298e9fc3c544eb1dea288b
Gaangb <angelob4stos@gmail.com>
2024-02-14 03:42:44 -0300

    feat: iniciando visao mobile para tela login, eventos e card de eventos

--- src/components/CardEvent/index.jsx
+++ src/components/CardEvent/index.jsx
@@ -5,12 +5,12 @@ export default function CardEvent({nome, local, data, horario, imagem, descricao
 
   return (
     <div className={styles.container_geral_events_page}>
-      <div>
+      <div className={styles.container_imagem_titulo_events_page}>
         <div className={styles.container_titulo_events_page}>
           <h1>{nome}</h1>
           <p>Local: {local}</p>
           <p>Data: {data}</p>
-          <p>Horario: {horario}</p>
+          <p className={styles.hide_on_phone}>Horario: {horario}</p>
         </div>
         <img
           src={imagem}
@@ -18,8 +18,8 @@ export default function CardEvent({nome, local, data, horario, imagem, descricao
         />
       </div>
       <div>
-      <p>
-        <b>Descrição:</b> {descricao}
+      <p className={styles.hide_on_phone}>
+        <b >Descrição:</b>{descricao}
       </p>
       </div>
     </div>
