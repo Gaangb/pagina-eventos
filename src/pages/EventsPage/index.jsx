bc69c319de192fe86c6f899ad2cb085d6ef2b48d
Gaangb <angelob4stos@gmail.com>
2024-02-16 01:25:59 -0300

    feat: alterando ícone da página de eventos

--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
+import { useEventsBuilder } from "../../hooks/useEventsBuilder";
+
 import CardEvent from "../../components/CardEvent";
 import styles from "./styles.module.css";
-import { useEventsBuilder } from "../../hooks/useEventsBuilder";
-import { useEffect } from "react";
+import CelebrationOutlinedIcon from '@mui/icons-material/CelebrationOutlined';
+
 export function EventsPage() {
   const { eventos, setEventos} = useEventsBuilder();
 
@@ -19,7 +22,7 @@ export function EventsPage() {
   return (
     <section className={styles.container_geral_events_page}>
       <div className={`${styles.container_events_page} `}>
-        <h1><ion-icon name="ticket-outline"></ion-icon>Todos os eventos</h1>
+        <h1><CelebrationOutlinedIcon />Todos os eventos</h1>
         <div className={styles.container_cards_events_page}>
           {eventos.map((event, index) => (
             <CardEvent key={index} {...event} />
