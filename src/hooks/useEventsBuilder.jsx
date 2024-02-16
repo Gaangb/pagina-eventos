10940ae2bb3db6e6dfd39fed587f03cf0bab9ef1
Gaangb <angelob4stos@gmail.com>
2024-02-16 01:11:30 -0300

    feat: criando lógica para deletar evento e arquivo utils

--- src/hooks/useEventsBuilder.jsx
+++ src/hooks/useEventsBuilder.jsx
@@ -1,80 +1,20 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, useCallback } from "react";
+import { eventosPreDefinidos, usuariosPredefinidos } from "../utils/utils";
+
 import logo from "../assets/logo_login.png"
 
 const EventContext = createContext();
 
 export function EventProvider({ children }) {
-  const [loggedInUser, setLoggedInUser] = useState({});
-
-  const [isLogged, setIsLogged] = useState(false);
-
-  const [showForm, setShowForm] = useState(false)
-
   const [customClassNavBar, setCustomClassNavBar] = useState("container_navbar_events_page")
-
+  const [isLogged, setIsLogged] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState({});
+  const [showForm, setShowForm] = useState(false)
   const [showComponentsUserPage, setShowComponentsUserPage] = useState('UserAccount')
-
-
-  const usuariosPredefinidos = [
-    {
-      id: 1,
-      email: "administrador@gmail.com",
-      senha: "senha123",
-      cpf: "12345678900",
-      nome: "admin",
-      nome_estabelecimento: "Feira Eventos",
-      imagem: "https://cdn-icons-png.flaticon.com/512/149/149071.png"
-    }
-  ]
-
-  const eventosPreDefinidos = [
-    {
-      id: 1,
-      criador: 1,
-      nome: "Rock Rio",
-      local: "Rio de Janeiro",
-      data: "10/10/2021",
-      horario: "20:00",
-      imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
-      descricao: "Rock in Rio",
-      ingressos_pista: 100,
-      ingressos_camarote: 100,
-      preco_pista: 100,
-      preco_camarote: 100,
-    },
-    {
-      id: 2,
-      criador: 2,
-      nome: "Rock Rio",
-      local: "Rio de Janeiro",
-      data: "10/10/2021",
-      horario: "20:00",
-      imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
-      descricao: "Rock in Rio",
-      ingressos_pista: 100,
-      ingressos_camarote: 100,
-      preco_pista: 100,
-      preco_camarote: 100,
-    },
-    {
-      id: 3,
-      criador: 1,
-      nome: "Rock Rio",
-      local: "Rio de Janeiro",
-      data: "10/10/2021",
-      horario: "20:00",
-      imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
-      descricao: "Rock in Rio",
-      ingressos_pista: 100,
-      ingressos_camarote: 100,
-      preco_pista: 100,
-      preco_camarote: 100,
-    }
-  ];
 
   const [usuarios, setUsuarios] = useState(() => {
     const usuariosLocalStorage = JSON.parse(localStorage.getItem('usuarios'));
-  
+
     if (usuariosLocalStorage) {
       const idsLocalStorage = new Set(usuariosLocalStorage.map(usuario => usuario.id));
       const usuariosPredefinidosNovos = usuariosPredefinidos.filter(usuario => !idsLocalStorage.has(usuario.id));
@@ -86,7 +26,7 @@ export function EventProvider({ children }) {
 
   const [eventos, setEventos] = useState(() => {
     const eventosLocalStorage = JSON.parse(localStorage.getItem('eventos'));
-    
+
     if (eventosLocalStorage) {
       const idsLocalStorage = new Set(eventosLocalStorage.map(evento => evento.id));
       const eventosPreDefinidosNovos = eventosPreDefinidos.filter(evento => !idsLocalStorage.has(evento.id));
@@ -96,54 +36,55 @@ export function EventProvider({ children }) {
     }
   });
 
+  const showLogo = <img src={logo} alt="Logo" onClick={() => location.href = "/"} />
+
+  const deleteEvent = useCallback((id) => {
+    const updatedEvents = eventos.filter((event) => event.id !== id);
+    setEventos(updatedEvents);
+  }, [eventos]);
 
   useEffect(() => {
-    // let loggedInUser = null;
-
     const loggedInUserJSON = localStorage.getItem("loggedInUser");
-  
+
     if (loggedInUserJSON) {
-        // loggedInUser = JSON.parse(loggedInUserJSON);
-        setLoggedInUser(loggedInUserJSON);
-        setIsLogged(true)
-        console.log('loggedInUser', loggedInUser)
+      setLoggedInUser(loggedInUserJSON);
+      setIsLogged(true)
     }
   }, [isLogged, loggedInUser]);
-console.log(loggedInUser)
+
   const handleLogOut = () => {
     setIsLogged(false)
     localStorage.removeItem("loggedInUser");
     setTimeout(() => {
       location.reload();
     })
-}
+  }
 
   const toggleForm = (e) => {
     e.preventDefault()
     setShowForm(!showForm)
   }
 
-  const showLogo = <img src={logo} alt="Logo" onClick={ () => location.href = "/"}/>
-
   return (
     <EventContext.Provider value={{
-      usuarios,
-      setUsuarios,
-      eventos,
-      setEventos,
-      showForm,
-      setShowForm,
-      toggleForm,
-      isLogged,
-      setIsLogged,
-      showLogo,
-      handleLogOut,
       customClassNavBar,
-      setCustomClassNavBar,
+      eventos,
+      isLogged,
       loggedInUser,
       loggedInUserJSON: localStorage.getItem("loggedInUser"),
       showComponentsUserPage,
+      showForm,
+      showLogo,
+      usuarios,
+      setUsuarios,
+      setEventos,
+      setShowForm,
+      toggleForm,
+      setIsLogged,
+      handleLogOut,
+      setCustomClassNavBar,
       setShowComponentsUserPage,
+      deleteEvent,
     }}>
       {children}
     </EventContext.Provider>
---
+++ src/utils/utils.js
@@ -0,0 +1,56 @@
+export const eventosPreDefinidos = [
+    {
+        id: 1,
+        usuarioId: 1,
+        nome: "Rock Rio",
+        local: "Rio de Janeiro",
+        data: "10/10/2021",
+        horario: "20:00",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
+        descricao: "Rock in Rio",
+        ingressos_pista: 100,
+        ingressos_camarote: 100,
+        preco_pista: 100,
+        preco_camarote: 100,
+    },
+    {
+        id: 2,
+        usuarioId: 2,
+        nome: "Rock Rio",
+        local: "Rio de Janeiro",
+        data: "10/10/2021",
+        horario: "20:00",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
+        descricao: "Rock in Rio",
+        ingressos_pista: 100,
+        ingressos_camarote: 100,
+        preco_pista: 100,
+        preco_camarote: 100,
+    },
+    {
+        id: 3,
+        usuarioId: 1,
+        nome: "Rock Rio",
+        local: "Rio de Janeiro",
+        data: "10/10/2021",
+        horario: "20:00",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Rock_in_Rio_-_Madrid_2012.jpg",
+        descricao: "Rock in Rio",
+        ingressos_pista: 100,
+        ingressos_camarote: 100,
+        preco_pista: 100,
+        preco_camarote: 100,
+    }
+];
+
+export const usuariosPredefinidos = [
+    {
+        id: 1,
+        email: "administrador@gmail.com",
+        senha: "senha123",
+        cpf: "12345678900",
+        nome: "admin",
+        nome_estabelecimento: "Feira Eventos",
+        imagem: "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+    }
+]
