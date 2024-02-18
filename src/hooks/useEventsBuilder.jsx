19063fb04ba77974ec10ed29a98325f437d7d868
Gaangb <angelob4stos@gmail.com>
2024-02-18 19:19:07 -0300

    ref: organizando codigo do hooks

--- src/hooks/useEventsBuilder.jsx
+++ src/hooks/useEventsBuilder.jsx
@@ -1,23 +1,43 @@
-import { createContext, useContext, useEffect, useState, useCallback } from "react";
-import { eventosPreDefinidos, loggedInUserJSON, usuariosPredefinidos } from "../utils/utils";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+} from "react";
+import {
+  eventosPreDefinidos,
+  loggedInUserJSON,
+  usuariosPredefinidos,
+} from "../utils/utils";
 
-import logo from "../assets/logo_login.png"
+import logo from "../assets/logo_login.png";
 
 const EventContext = createContext();
 
 export function EventProvider({ children }) {
-  const [customClassNavBar, setCustomClassNavBar] = useState("container_navbar_events_page")
+  const [currentEvent, setCurrentEvent] = useState({});
+
+  const [customClassNavBar, setCustomClassNavBar] = useState(
+    "container_navbar_events_page"
+  );
+
   const [isLogged, setIsLogged] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState({});
-  const [showForm, setShowForm] = useState(false)
-  const [showComponentsUserPage, setShowComponentsUserPage] = useState('UserAccount')
-
+  const [showForm, setShowForm] = useState(false);
+  const [showComponentsUserPage, setShowComponentsUserPage] =
+    useState("UserAccount");
+  
   const [usuarios, setUsuarios] = useState(() => {
-    const usuariosLocalStorage = JSON.parse(localStorage.getItem('usuarios'));
+    const usuariosLocalStorage = JSON.parse(localStorage.getItem("usuarios"));
 
     if (usuariosLocalStorage) {
-      const idsLocalStorage = new Set(usuariosLocalStorage.map(usuario => usuario.id));
-      const usuariosPredefinidosNovos = usuariosPredefinidos.filter(usuario => !idsLocalStorage.has(usuario.id));
+      const idsLocalStorage = new Set(
+        usuariosLocalStorage.map((usuario) => usuario.id)
+      );
+      const usuariosPredefinidosNovos = usuariosPredefinidos.filter(
+        (usuario) => !idsLocalStorage.has(usuario.id)
+      );
       return [...usuariosPredefinidosNovos, ...usuariosLocalStorage];
     } else {
       return usuariosPredefinidos;
@@ -25,65 +45,78 @@ export function EventProvider({ children }) {
   });
 
   const [eventos, setEventos] = useState(() => {
-    const eventosLocalStorage = JSON.parse(localStorage.getItem('eventos'));
+    const eventosLocalStorage = JSON.parse(localStorage.getItem("eventos"));
 
     if (eventosLocalStorage) {
-      const idsLocalStorage = new Set(eventosLocalStorage.map(evento => evento.id));
-      const eventosPreDefinidosNovos = eventosPreDefinidos.filter(evento => !idsLocalStorage.has(evento.id));
+      const idsLocalStorage = new Set(
+        eventosLocalStorage.map((evento) => evento.id)
+      );
+      const eventosPreDefinidosNovos = eventosPreDefinidos.filter(
+        (evento) => !idsLocalStorage.has(evento.id)
+      );
       return [...eventosPreDefinidosNovos, ...eventosLocalStorage];
     } else {
       return eventosPreDefinidos;
     }
   });
 
-  const showLogo = <img src={logo} alt="Logo" onClick={() => location.href = "/"} />
+  const showLogo = (
+    <img src={logo} alt="Logo" onClick={() => (location.href = "/")} />
+  );
 
-  const deleteEvent = useCallback((id) => {
-    const updatedEvents = eventos.filter((event) => event.id !== id);
-    setEventos(updatedEvents);
-  }, [eventos]);
+  const deleteEvent = useCallback(
+    (id) => {
+      const updatedEvents = eventos.filter((event) => event.id !== id);
+      setEventos(updatedEvents);
+    },
+    [eventos]
+  );
 
   useEffect(() => {
     if (loggedInUserJSON) {
       setLoggedInUser(loggedInUserJSON);
-      setIsLogged(true)
+      setIsLogged(true);
     }
   }, [isLogged, loggedInUser]);
 
   const handleLogOut = () => {
-    setIsLogged(false)
+    setIsLogged(false);
     localStorage.removeItem("loggedInUser");
     setTimeout(() => {
       location.reload();
-    })
-  }
+    });
+  };
 
   const toggleForm = (e) => {
-    e.preventDefault()
-    setShowForm(!showForm)
-  }
+    e.preventDefault();
+    setShowForm(!showForm);
+  };
 
   return (
-    <EventContext.Provider value={{
-      customClassNavBar,
-      eventos,
-      isLogged,
-      loggedInUser,
-      loggedInUserJSON: localStorage.getItem("loggedInUser"),
-      showComponentsUserPage,
-      showForm,
-      showLogo,
-      usuarios,
-      setUsuarios,
-      setEventos,
-      setShowForm,
-      toggleForm,
-      setIsLogged,
-      handleLogOut,
-      setCustomClassNavBar,
-      setShowComponentsUserPage,
-      deleteEvent,
-    }}>
+    <EventContext.Provider
+      value={{
+        currentEvent,
+        customClassNavBar,
+        eventos,
+        isLogged,
+        loggedInUser,
+        loggedInUserJSON: localStorage.getItem("loggedInUser"),
+        showComponentsUserPage,
+        showForm,
+        showLogo,
+        usuarios,
+        setCurrentEvent,
+        setUsuarios,
+        setEventos,
+        setShowForm,
+        toggleForm,
+        setIsLogged,
+        handleLogOut,
+        setCustomClassNavBar,
+        setShowComponentsUserPage,
+        deleteEvent,
+      }}
+    >
       {children}
     </EventContext.Provider>
   );
