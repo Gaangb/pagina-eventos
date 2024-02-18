873b63a53e05d5ad9b8a1562e56d25388c58bd4f
Gaangb <angelob4stos@gmail.com>
2024-02-17 23:15:29 -0300

    feat: adicionando icones a pagina detalhes evento

--- src/pages/EventsDetailsPage/index.jsx
+++ src/pages/EventsDetailsPage/index.jsx
@@ -4,37 +4,51 @@ import { useLocation } from "react-router-dom";
 
 import styles from "./styles.module.css";
 import { PaymentContainer } from "../../components/TicketContainer";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import RoomOutlinedIcon from "@mui/icons-material/RoomOutlined";
+import PersonPinOutlinedIcon from '@mui/icons-material/PersonPinOutlined';
 
 export function EventsDetailsPage() {
+  const { eventos, setEventos, usuarios } = useEventsBuilder();
+  const location = useLocation();
+  const currentEvents = location.state.evento;
 
-    const { eventos, setEventos, usuarios } = useEventsBuilder();
-    const location = useLocation();
-    const currentEvents = location.state.evento;
+  const donoEvento = usuarios.filter(
+    (user) => user.id === currentEvents.usuarioId
+  );
 
-    const donoEvento = usuarios.filter((user) => user.id === currentEvents.usuarioId)
-
-    return (
-        <div className={styles.container_geral}>
-            <div className={styles.container_content}>
-                <div className={styles.container_content_top}>
-                    <img src={currentEvents.imagem} alt="imagem do evento" />
-                    <div className={styles.container_content_top_text}>
-                        <div>
-                            <h1>{currentEvents.nome}</h1>
-                            <p>Produzido por: {donoEvento[0].nome_estabelecimento}</p>
-                        </div>
-                        <p>{currentEvents.data}</p>
-                        <p>{currentEvents.horario}</p>
-                    </div>
-                </div>
-                <div className={styles.container_content_bottom}>
-                    <div className={styles.content_bottom_description}>
-                        <h2>Sobre o evento</h2>
-                        <p>{currentEvents.descricao}</p>
-                    </div>
-                    <PaymentContainer local={currentEvents.local} preco_camarote={currentEvents.preco_camarote} preco_pista={currentEvents.preco_pista}/>
-                </div>
+  return (
+    <div className={styles.container_geral} >
+      <div className={styles.container_content}>
+        <div className={styles.container_content_top}>
+          <img src={currentEvents.imagem} alt="imagem do evento" />
+          <div className={styles.container_content_top_text}>
+            <div>
+              <h1>{currentEvents.nome}</h1>
+              <p> <PersonPinOutlinedIcon /> {donoEvento[0].nome_estabelecimento}</p>
             </div>
+            <div>
+              <p>
+                <RoomOutlinedIcon /> {currentEvents.local}
+              </p>
+              <p>
+                <CalendarMonthOutlinedIcon />
+                {currentEvents.data} - {currentEvents.horario}
+              </p>
+            </div>
+          </div>
         </div>
-    )
+        <div className={styles.container_content_bottom}>
+          <div className={styles.content_bottom_description}>
+            <h2>Sobre o evento</h2>
+            <p>{currentEvents.descricao}</p>
+          </div>
+          <PaymentContainer
+            preco_camarote={currentEvents.preco_camarote}
+            preco_pista={currentEvents.preco_pista}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
