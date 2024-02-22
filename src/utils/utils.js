a0f46409cced1e7c5340a66b1dacec5bc8dd1f46
Gaangb <angelob4stos@gmail.com>
2024-02-21 21:08:28 -0300

    fix: corrigindo bug na data

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -61,6 +61,6 @@ export function formatDateForInput(dateString) {
     const dateObject = new Date(dateString);
     const year = dateObject.getFullYear();
     const month = String(dateObject.getMonth() + 1).padStart(2, '0');
-    const day = String(dateObject.getDate()).padStart(2, '0');
+    const day = String(dateObject.getDate() + 1).padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
