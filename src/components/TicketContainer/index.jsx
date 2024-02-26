bbb8d0bd18e19a47e3ee76f662743bbd3d8ec44f
Gaangb <angelob4stos@gmail.com>
2024-02-26 19:54:50 -0300

    feat: adicionando logica para compra de ingressos quando indisponivel

--- src/components/TicketContainer/index.jsx
+++ src/components/TicketContainer/index.jsx
@@ -4,42 +4,55 @@ import { useNavigate } from "react-router-dom";
 
 import styles from "./styles.module.css";
 import QuantityInput from "../QuantityInput";
-
+import { useEventsBuilder } from "../../hooks/useEventsBuilder";
 export function PaymentContainer({ preco_pista, preco_camarote, evento }) {
+  const { eventos } = useEventsBuilder();
   const navigate = useNavigate();
   const [purshaseDetails, setPurshaseDetails] = useState({
+    event_id: evento.id,
     quantityPista: 0,
     quantityCamarote: 0,
     payment: 0,
-    evento: evento,
-  })
+    evento: evento.nome,
+    total_pista: evento.ingressos_pista,
+    total_camarote: evento.ingressos_camarote,
+  });
 
   const handleQuantityChangePista = (newQuantity) => {
     setPurshaseDetails({
       ...purshaseDetails,
-      quantityPista: newQuantity
-    })
+      quantityPista: newQuantity,
+    });
   };
 
   const handleQuantityChangeCamarote = (newQuantity) => {
     setPurshaseDetails({
       ...purshaseDetails,
-      quantityCamarote: newQuantity
-    })
+      quantityCamarote: newQuantity,
+    });
   };
 
   const handleBuyTickets = () => {
-    navigate("/pagamento", { state: { purshaseDetails } });
+    if (
+      evento.ingressos_camarote < purshaseDetails.quantityCamarote || evento.ingressos_pista < purshaseDetails.quantityPista
+    ){
+      alert("Quantidade indisponível")
+    }else{
+      navigate("/pagamento", { state: { purshaseDetails } });
+      
+    }
   };
 
   useEffect(() => {
     const total =
-    purshaseDetails.quantityPista * preco_pista + purshaseDetails.quantityCamarote * preco_camarote;
+      purshaseDetails.quantityPista * preco_pista +
+      purshaseDetails.quantityCamarote * preco_camarote;
     setPurshaseDetails({
       ...purshaseDetails,
-      payment: total
+      payment: total,
     });
-  },[purshaseDetails.quantityPista, purshaseDetails.quantityCamarote]);
+
+  }, [purshaseDetails.quantityPista, purshaseDetails.quantityCamarote]);
 
   return (
     <div className={styles.container_geral}>
