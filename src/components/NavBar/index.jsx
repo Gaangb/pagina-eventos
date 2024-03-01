b35e86b0445b406d77102faf7b5fbbf2869b7b1e
Gaangb <angelob4stos@gmail.com>
2024-02-29 22:22:33 -0300

    feat: importando o componente de search

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,65 +1,112 @@
-import styles from "./styles.module.css";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 import { useEventsBuilder } from "../../hooks/useEventsBuilder";
-import logo from "../../assets/logo.png"
-import { CreateEvent } from "../CreateEvent";
+import { CreateEventModal } from "../CreateEventModal";
+import { InputSearch } from "../InputSearch";
 
+import logo from "../../assets/logo.png";
+import styles from "./styles.module.css";
 
 export function NavBar() {
-    const { toggleForm, isLogged, handleLogOut, showForm, customClassNavBar, showNavbar } = useEventsBuilder();
+  const {
+    eventos,
+    toggleForm,
+    isLogged,
+    handleLogOut,
+    showForm,
+    customClassNavBar,
+    showNavbar,
+    filteredEvents,
+    setFilteredEvents,
+    setIsFiltered,
+  } = useEventsBuilder();
 
-    const handleCreateEvent = (e) => {
-        toggleForm(e)
+  const handleCreateEventModal = (e) => {
+    toggleForm(e);
+  };
+
+  const onInputChange = (e) => {
+    const value = e.target.value.toLowerCase();
+    if (!value.trim()) {
+      setFilteredEvents(eventos);
+      return;
     }
 
-    function renderButtons() {
-        if (isLogged && showNavbar) {
-            return (
-                <>
-                    <input type="text" name="" id="" placeholder="Pesquise" />
-                    <button className={styles.button_navbar_events_page} onClick={(e) => { handleCreateEvent(e) }}>Criar evento</button>
-                    <button className={styles.button_navbar_events_page} onClick={() => { location.href = '/minha-conta' }}>Minha conta</button>
-                    <button className={styles.button_sair_navbar_events_page} onClick={() => { handleLogOut() }}>Sair</button>
-                </>
-            )
-        } else if(!isLogged && showNavbar) {
-            return (
-                <>
-                    <input type="text" name="" id="" placeholder="Pesquise" />
-                    <button className={styles.button_login_navbar_events_page} onClick={() => location.href = '/register'}>Criar conta</button>
-                    <button className={styles.button_login_navbar_events_page} onClick={() => location.href = '/login'}>Login</button>
-                </>
-            )
-        } else {
-            return null
-        }
-     }
-    // const buttons = isLogged && showNavbar ?(
-    //     <>
-    //         <button className={styles.button_navbar_events_page} onClick={(e) => { handleCreateEvent(e) }}>Criar evento</button>
-    //         <button className={styles.button_navbar_events_page} onClick={() => { location.href = '/minha-conta' }}>Minha conta</button>
-    //         <button className={styles.button_sair_navbar_events_page} onClick={() => { handleLogOut() }}>Sair</button>
-    //     </>
-    // ) : (
-    //     <>
-    //         <button className={styles.button_login_navbar_events_page} onClick={() => location.href = '/register'}>Criar conta</button>
-    //         <button className={styles.button_login_navbar_events_page} onClick={() => location.href = '/login'}>Login</button>
-    //     </>
-    // );
+    const filteredEvents = eventos.filter((event) => {
+      return event.nome.toLowerCase().includes(value);
+    });
+    setIsFiltered(true);
+    setFilteredEvents(filteredEvents);
+  };
 
-
-    return (
+  function renderButtons() {
+    if (isLogged && showNavbar) {
+      return (
         <>
-        <ToastContainer />
-
-            <div className={styles[customClassNavBar]}>
-                <img src={logo} alt="Logo" onClick={() => location.href = "/"} />
-                {renderButtons()}
-
-            </div>
-            <div className={`${styles.container_events_page} ${showForm ? styles.modal_open : ""}`}>
-                {showForm && <CreateEvent />}
-            </div>
+          <InputSearch onChange={onInputChange} />
+          <button
+            className={styles.button_navbar_events_page}
+            onClick={(e) => {
+              handleCreateEventModal(e);
+            }}
+          >
+            Criar evento
+          </button>
+          <button
+            className={styles.button_navbar_events_page}
+            onClick={() => {
+              location.href = "/minha-conta";
+            }}
+          >
+            Minha conta
+          </button>
+          <button
+            className={styles.button_sair_navbar_events_page}
+            onClick={() => {
+              handleLogOut();
+            }}
+          >
+            Sair
+          </button>
         </>
-    )
+      );
+    } else if (!isLogged && showNavbar) {
+      return (
+        <>
+          <InputSearch onChange={onInputChange} />
+          <button
+            className={styles.button_login_navbar_events_page}
+            onClick={() => (location.href = "/register")}
+          >
+            Criar conta
+          </button>
+          <button
+            className={styles.button_login_navbar_events_page}
+            onClick={() => (location.href = "/login")}
+          >
+            Login
+          </button>
+        </>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  return (
+    <>
+      <ToastContainer />
+
+      <div className={styles[customClassNavBar]}>
+        <img src={logo} alt="Logo" onClick={() => (location.href = "/")} />
+        {renderButtons()}
+      </div>
+      <div
+        className={`${styles.container_events_page} ${
+          showForm ? styles.modal_open : ""
+        }`}
+      >
+        {showForm && <CreateEventModal />}
+      </div>
+    </>
+  );
 }
