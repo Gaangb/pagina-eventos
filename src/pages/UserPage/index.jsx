4feb876b88325aaecf775bfb6ad949b62e804f27
Gaangb <angelob4stos@gmail.com>
2024-03-02 22:28:59 -0300

    feat: retirando navbar da pagina de usuarios e mantendo apenas a sidebar

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { MyEvents } from "../../components/MyEvents";
 import { SideBar } from "../../components/SideBar";
 import { UserAccount } from "../../components/UserAccount";
 import { useEventsBuilder } from "../../hooks/useEventsBuilder";
-
+import DehazeIcon from '@mui/icons-material/Dehaze';
 import styles from "./styles.module.css";
 
 export function UserPage() {
-  const { showComponentsUserPage } = useEventsBuilder();
+  const isMobile = useMediaQuery({ maxWidth: 767 }); // Define o limite para dispositivos móveis
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+  const { showComponentsUserPage, setCustomClassNavBar } = useEventsBuilder();
+
+  useEffect(() => {
+    setCustomClassNavBar("no_show_navbar");
+  }, []);
+
   const renderComponent = () => {
     if (showComponentsUserPage === "UserAccount") {
       return <UserAccount />;
@@ -15,9 +24,19 @@ export function UserPage() {
     }
   };
 
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
   return (
     <div className={styles.container_geral}>
-      <SideBar />
+
+      {sidebarVisible && <SideBar isMobile={isMobile} onClick={toggleSidebar}/>}
+      {isMobile && (
+        <button onClick={toggleSidebar} className={styles.button_sidebar}>
+          {sidebarVisible ? <DehazeIcon /> : <DehazeIcon />}
+        </button>
+      )}
       {renderComponent()}
     </div>
   );
